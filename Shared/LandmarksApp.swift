b942d42eaf8291f44c3bd92d7b48cb6fae2fb0e1
ryosk7 <uchiryo7@gmail.com>
2022-10-15 19:56:50 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by 内田涼介 on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
